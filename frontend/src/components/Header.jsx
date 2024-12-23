8bf45fc4588d0f00b22f8e32b55720240da5bf07
Misha Jain <mishajain1110@gmail.com>
2024-12-23 20:10:06 +0530

    user authentication

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Header.css";
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <h3>Home Page</h3>
+            </div>
+           
+        </header>
+    )
+}
+
+export default Header;

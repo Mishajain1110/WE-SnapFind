88d597893d4ac65d941f780b15578220d18b0ee9
Durga Nandini Ch <dnandinich@gmail.com>
2025-02-03 19:36:50 +0530

    Updated signup

--- lost-and-found/accounts/forms.py
+++ lost-and-found/accounts/forms.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class SignupForm(UserCreationForm):
-
+    ALLOWED_EMAILS = {
+        "vrsec.ac.in", "iiti.ac.in", "pesu.pes.edu", "vnrvjiet.in"  # Add more institutional domains
+    }
+    TEAM_EMAILS = {
+        "dnandinich@gmail.com", "mishajain1110@gmail.com", "praneethakalbhavi@gmail.com", "sirivoore249@gmail.com"
+    }
+    
     username = forms.CharField(
-        widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Roll Number'}),
-        label = 'Roll Number'
+        widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Username'}),
+        label = 'Username'
     )
     first_name = forms.CharField(
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
@@ -32,6 +39,16 @@ class SignupForm(UserCreationForm):
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
         label='Confirm Password'
     )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        domain = email.split("@")[-1]
+
+        if domain not in self.ALLOWED_EMAILS and email not in self.TEAM_EMAILS:
+            raise ValidationError("Only institutional or team member emails are allowed.")
+
+        return email
+    
     class Meta:
         model = User
         fields = ("username", "password1", "password2", "first_name", "last_name", "email")
